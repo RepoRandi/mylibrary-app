9d73fdd37cf7640c31095301339486a16e78a4de
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-07-17 11:02:29 +0700

    Implementasi React-Redux

--- src/App.js
+++ src/App.js
@@ -25,12 +25,16 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './routes';
+import {Provider} from 'react-redux';
+import {store} from './redux';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  name: 'Randi Maulana Akbar',
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
